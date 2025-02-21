42b8f5c087f9336e7544196d53afc6af57cd37d9
Kristofer Peterson <svenski123@users.noreply.github.com>
2025-02-21 19:19:48 +0000

    hard code total supply as constant, eliminate storage slot assignment during construction, fix up owner balance dynamically

--- src/Gas.sol
+++ src/Gas.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract GasContract {
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) private the_balances;
     uint256 whiteListAmount;
     address immutable admin0;
     address immutable admin1;
     address immutable admin2;
     address immutable admin3;
     address constant admin4 = address(0x1234);
+    uint256 constant totalSupply = 1000000000;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
@@ -18,12 +19,11 @@ contract GasContract {
         _;
     }
 
-    constructor(address[] memory _admins, uint256 totalSupply) payable {
+    constructor(address[] memory _admins, uint256) payable {
         admin0 = _admins[0];
         admin1 = _admins[1];
         admin2 = _admins[2];
         admin3 = _admins[3];
-        balances[admin4] = totalSupply;
     }
 
     function administrators(uint256 _index) external view returns (address admin_) {
@@ -39,7 +39,17 @@ contract GasContract {
     }
 
     function balanceOf(address _user) public view returns (uint256 balance_) {
-        return balances[_user];
+        balance_ = the_balances[_user];
+        if (_user == admin4) {
+            unchecked { balance_ += totalSupply; }
+        }
+    }
+
+    function balances(address _user) public view returns (uint256 balance_) {
+        balance_ = the_balances[_user];
+        if (_user == admin4) {
+            unchecked { balance_ += totalSupply; }
+        }
     }
 
     function whitelist(address) external pure returns (uint256) {}
@@ -57,8 +67,8 @@ contract GasContract {
         uint256 _amount
     ) private {
         // balance must be checked for correctness however we are favouring optimisation over correctness in this exercide
-        unchecked { balances[msg.sender] -= _amount; }
-        unchecked { balances[_recipient] += _amount; }
+        unchecked { the_balances[msg.sender] -= _amount; }
+        unchecked { the_balances[_recipient] += _amount; }
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier)
